7a42a891b1ffd594337150fd6ab5445fe0f62b33
Subhojit Konar <koner.subhojit@gmail.com>
2018-07-25 17:20:58 +0530

    3rd

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.get('/',function(req , res){
-    res.end("Hello");
+const pug = require('pug');
+
+const message = pug.compileFile('message.pug');
+
+app.get('*',function(req , res){
+    res.end(message({name: 'Subhojit'}));
     console.log(req.path);
 });
 
